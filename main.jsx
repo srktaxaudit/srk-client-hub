d4afa9fc3ed77ef9e0f34a1350f01009f7358dea
srktaxaudit <srktaxaudit5@gmail.com>
2026-04-03 13:44:29 +0530

    Add admin route handling in main.jsx

--- main.jsx
+++ main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import AdminApp from './admin.jsx'
+
+const isAdmin = window.location.pathname.startsWith('/admin')
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    {isAdmin ? <AdminApp /> : <App />}
+  </React.StrictMode>
 )
